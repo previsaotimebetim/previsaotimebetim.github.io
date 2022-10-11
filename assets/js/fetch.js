526074df9275e96bedd32071b3557705ebd8453a
Alexsander Silva <alexsander.silva14@live.com>
2022-10-10 22:59:27 -0300

    Primeio commit

---
+++ assets/js/fetch.js
@@ -0,0 +1,113 @@
+const tempo = document.querySelector('#titulo');
+const cidade = document.querySelector('#cidade')
+const botao = document.querySelector('#botao');
+const previsao = async () =>{
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default',
+    };
+
+    // const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=sao&paulo&appid=f72255d6f2aeed89f1c023d7665aba87&units=metric&lang=pt_br', options);
+    //const response = await fetch('https://api.hgbrasil.com/weather?format=json-cors&key=53e7a432&city_name=Campinas,SP', options);
+    const response = await fetch('https://api.hgbrasil.com/weather?format=json-cors&key=53e7a432&user_ip=remote', options);
+    const json = await response.json();
+    tempo.innerHTML = `Temperatura atual  ${json.results.city_name}  (${json.results.temp})°C`;
+    for(let i = 1; i < json.results.forecast.length - 1; i++){
+        const div = document.createElement('div');
+        div.setAttribute('class', 'container');
+        let condition = json.results.forecast[i].condition;
+        if(condition === 'storm'){
+            condition = 'tempestade.png';
+        }
+        else if(condition === 'clear_day' || condition === 'clear_night'){
+            condition = 'limpo.png';
+        }
+        else if(condition === 'rain'){
+            condition = 'chuva.png';
+        }
+        else if(condition === 'cloud' || condition === 'cloudly_day' || condition === 'cloudly_night'){
+            condition = 'nublado.png';
+        }
+        else if(condition === 'fog'){
+            condition = 'neblina.png';
+        }
+        else if(condition === 'hail'){
+            condition = 'granizo.png';
+        }
+        else{
+            condition = 'erro.png';
+        }
+        div.innerHTML = `<div> 
+                <div class="card"> 
+                    <h2>${json.results.forecast[i].weekday}</h2>
+                    <p>${json.results.forecast[i].date}</p>
+                    <p>${json.results.forecast[i].min}º/${json.results.forecast[i].max}º</p>
+                    <img src="/assets/images/${condition}" alt="" width='64' heigth='64'>
+                    <p>${json.results.forecast[i].description}</p>
+                    <p class="att">Velocidade do Vento ${json.results.forecast[i].wind_speedy}<br>Probabilidade de Chuva ${json.results.forecast[i].rain_probability}%</p>
+
+                 </div> 
+            </div>`
+        document.querySelector('main').appendChild(div);   
+    }
+   
+}
+
+previsao();
+
+botao.addEventListener('click', async (e) =>{
+    for(let i = 0; i < 8; i++){
+        document.querySelector('.container').remove();
+    }
+
+
+    let search = cidade.value.replace('-', ',');
+    //search = search.value.replace(' ', '-');
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default',
+    }
+    const response = await fetch(`https://api.hgbrasil.com/weather?format=json-cors&key=53e7a432&city_name=${search}`, options);
+    const json = await response.json();
+    tempo.innerHTML = `Tempo atual ${json.results.city_name}  (${json.results.temp})°C`;
+    for(let i = 1; i < json.results.forecast.length - 1; i++){
+        const div = document.createElement('div');
+        div.setAttribute('class', 'container');
+        let condition = json.results.forecast[i].condition;
+        if(condition === 'storm'){
+            condition = 'tempestade.png';
+        }
+        else if(condition === 'clear_day' || condition === 'clear_night'){
+            condition = 'limpo.png';
+        }
+        else if(condition === 'rain'){
+            condition = 'chuva.png';
+        }
+        else if(condition === 'cloud' || condition === 'cloudly_day' || condition === 'cloudly_night'){
+            condition = 'nublado.png';
+        }
+        else if(condition === 'fog'){
+            condition = 'neblina.png';
+        }
+        else if(condition === 'hail'){
+            condition = 'granizo.png';
+        }
+        else{
+            condition = 'erro.png';
+        }
+        div.innerHTML = `<div> 
+                <div class="card"> 
+                    <h2>${json.results.forecast[i].weekday}</h2>
+                    <p>${json.results.forecast[i].date}</p>
+                    ${json.results.forecast[i].min}º/${json.results.forecast[i].max}º
+                    <img src="/assets/images/${condition}" alt="" width='64' heigth='64'>
+                    ${json.results.forecast[i].description}
+                    <p>${json.results.city_name}</p>
+                    <p class="att">Velocidade do Vento ${json.results.forecast[i].wind_speedy}<br>Probabilidade de Chuva ${json.results.forecast[i].rain_probability}%</p>
+                 </div> 
+            </div>`
+        document.querySelector('main').appendChild(div);   
+    }
+})
